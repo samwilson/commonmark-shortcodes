4a5bb85986a59459473ea9afbe7eee89a0000d69
Sam Wilson <sam@samwilson.id.au>
2023-04-16 21:00:46 +0800

    Simplify attr fetching

--- src/Shortcode.php
+++ src/Shortcode.php
@@ -38,9 +38,8 @@ class Shortcode extends AbstractBlock implements RawMarkupContainerInterface
         $doc->loadHTML('<body ' . $string . ' />');
         $body = $doc->getElementsByTagName('body')->item(0);
         for ($i = 0; $i < $body->attributes->length; ++$i) {
-            $name  = $body->attributes->item($i)->nodeName;
-            $value = $body->attributes->getNamedItem($name)->nodeValue;
-            $this->setAttr($name, $value);
+            $attr = $body->attributes->item($i);
+            $this->setAttr($attr->nodeName, $attr->nodeValue);
         }
     }
 
